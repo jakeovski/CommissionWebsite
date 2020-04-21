09cd0289ae3ee224f91553ead4cab06345ac9cf5
Captain Sir <jakeovski@gmail.com>
2020-04-22 02:12:15 +0300

    start

---
+++ js/searchDisplay.js
@@ -0,0 +1,77 @@
+
+function x(){
+
+
+};
+
+
+function displayCorrectSearches(){
+
+  $('#searchResultsBox').append(
+
+    '<div class="container">'+
+        '<div class="row" style="display: flex; align-items: flex-start;">'+
+
+        //First Item
+        '<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 border border-dark rounded">'+
+            '<h3 id="Artist" class=" d-flex justify-content-center" style="margin-bottom: 20px;"> WLOP</h3>'+
+            '<ul class="social-network social-circle d-flex justify-content-center">'+
+                '<li><a href="#" class="icoInstagram" title="Instagram"> <i class="fa fa-instagram"></i></a></li>'+
+                '<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>'+
+                '<li><a href="#" class="icoDeviant" title="DeviantArt"><i class="fa fa-deviantart"></i></a></li>'+
+            '</ul>'+
+            '<div class="container" style="margin-top: 40px;">'+
+                '<div class="row" style="display: flex; align-items: flex-end;">'+
+                    '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'+
+                        '<img class="img-fluid rounded float-left img-thumbnail border-warning" src="Images/temporaryImages/thumb1.jpg" alt="Drawing example" id="myImg">'  +
+                    '</div>'+
+                    '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'+
+                        '<img class="img-fluid rounded float-right img-thumbnail border-warning" src="Images/temporaryImages/thumb2.jpg" alt="Drawing example" id="myImg2">'   +
+                    '</div>'+
+                    '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'+
+                        '<img class="img-fluid rounded float-left img-thumbnail border-warning" src="Images/temporaryImages/thumb3.jpg" alt="Drawing example" id="myImg3">'+
+                    '</div>'+
+                    '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'+
+                        '<img class="img-fluid rounded float-right img-thumbnail border-warning" src="Images/temporaryImages/thumb4.jpg" alt="Drawing example" id="myImg4">'+
+                    '</div>'+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+
+        //Gap
+        '<div class="col-lg-2 col-md-2">'+
+        '</div>'+
+
+        //SecondItem
+        '<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 border border-dark rounded">'+
+            '<h3 id="Artist" class=" d-flex justify-content-center" style="margin-bottom: 20px;"> WLOP</h3>'+
+            '<ul class="social-network social-circle d-flex justify-content-center">'+
+                '<li><a href="#" class="icoInstagram" title="Instagram"> <i class="fa fa-instagram"></i></a></li>'+
+                '<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>'+
+                '<li><a href="#" class="icoDeviant" title="DeviantArt"><i class="fa fa-deviantart"></i></a></li>'+
+            '</ul>'+
+            '<div class="container" style="margin-top: 40px;">'+
+                '<div class="row" style="display: flex; align-items: flex-end;">'+
+                    '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'+
+                        '<img class="img-fluid rounded float-left img-thumbnail border-warning" src="Images/temporaryImages/thumb1.jpg" alt="Drawing example" id="myImg">'  +
+                    '</div>'+
+                    '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'+
+                        '<img class="img-fluid rounded float-right img-thumbnail border-warning" src="Images/temporaryImages/thumb2.jpg" alt="Drawing example" id="myImg2">'   +
+                    '</div>'+
+                    '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'+
+                        '<img class="img-fluid rounded float-left img-thumbnail border-warning" src="Images/temporaryImages/thumb3.jpg" alt="Drawing example" id="myImg3">'+
+                    '</div>'+
+                    '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'+
+                        '<img class="img-fluid rounded float-right img-thumbnail border-warning" src="Images/temporaryImages/thumb4.jpg" alt="Drawing example" id="myImg4">'+
+                    '</div>'+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+
+
+      '</div>'+
+    '</div>'
+
+  );
+
+};
