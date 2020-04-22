8bee495396e37a2fca22a870edcfae94661455d9
Captain Sir <jakeovski@gmail.com>
2020-04-22 23:47:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/profiles";
+const url = "mongodb://localhost:27017/exposure";
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
