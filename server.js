ae2075d75b5738673c6f94af911bc94900aa8527
Captain Sir <jakeovski@gmail.com>
2020-04-22 20:24:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 //Main Page
 app.get('/', function(req,res) {
-    res.render('pages/index');
+    res.render('public/views/pages/index');
 });
 
 app.listen(8080);
