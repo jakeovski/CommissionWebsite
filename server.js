c0f6bbc0b2661af1a2db1db6d96bfbe771ed0222
Captain Sir <jakeovski@gmail.com>
2020-04-22 22:47:11 +0300

    Added Database

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/profiles";
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const app = express();
 
+//Using sessions
+app.use(session({secret : 'example'}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//Setting the view engine to ejs
 app.set('view engine', 'ejs');
+
+//Database
+var db;
+
+//Connection to mongo db
+MongoClient.connect(url,function(err,database) {
+    if(err) throw err;
+    db = database;
+    app.listen(8080);
+    console.log('Listening on 8080');
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -24,5 +47,3 @@ app.get('/login', function(req,res) {
 app.get('/register',function(req,res) {
     res.render('pages/reg');
 });
-
-app.listen(8080);
