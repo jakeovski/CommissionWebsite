ac85e68c74fed2f49a16d3aef5de6d8c08fd2910
Captain Sir <jakeovski@gmail.com>
2020-04-24 00:33:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,9 @@ app.get('/MainPage',function(req,res) {
 //About Route
 app.get('/about',function(req,res) {
     if(!req.session.loggedin){res.render('/pages/about');return;}
-    res.render('pages/about2');
+    res.render('pages/about2', {
+        currentUser : currentUser
+    });
 });
 
 //Login Route
