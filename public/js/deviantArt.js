d62147d9569faeb731a3caafd3a472032ef236d1
Captain Sir <jakeovski@gmail.com>
2020-04-29 01:05:13 +0300

    Update deviantArt.js

--- public/js/deviantArt.js
+++ public/js/deviantArt.js
@@ -10,15 +10,16 @@ function getAccessToken(searchItem) {
   var url = "https://www.deviantart.com/oauth2/token?grant_type=client_credentials&client_id=12052&client_secret=13ae1cb7fdfb9753668db6e2310c9323";
   $.ajax({
     url: "https://www.deviantart.com/oauth2/token?grant_type=client_credentials&client_id=12052&client_secret=13ae1cb7fdfb9753668db6e2310c9323",
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    type: "GET", /* or type:"GET" or type:"PUT" */
-    dataType: "json",
+    // headers: {
+    //   'Content-Type': 'application/x-www-form-urlencoded'
+    // },
+    // type: "GET", /* or type:"GET" or type:"PUT" */
+    dataType: "jsonp",
     data: {
     },
     success: function (result) {
-        printJSON(jsondata);
+        // printJSON(jsondata);
+        console.log(result)
     },
     error: function () {
       console.log("error");
