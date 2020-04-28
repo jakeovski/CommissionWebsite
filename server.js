f0af53aa187fb70aeca4071191e31924e0ffabb9
Captain Sir <jakeovski@gmail.com>
2020-04-28 05:51:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/delete',function(req,res) {
 app.post('/results',function(req,res) {
     //Test for the API\
     var output = "";
-    deviantnode.getPopularDeviations(clientid,clientSecret).then(response => output = response);
+    deviantnode.getPopularDeviations(clientid,clientSecret).then(response => output = JSON.stringify(response));
     res.send(output);
 
 })
