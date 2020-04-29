23c7f80a1823f1e1055d661b35194046f23e5201
Captain Sir <jakeovski@gmail.com>
2020-04-29 21:40:55 +0300

    next

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ app.post('/results', function (req, res) {
 
         return new Promise(function (resolve, reject) {
 
-            request('https://www.deviantart.com/api/v1/oauth2/browse/popular?category_path=digitalart%2Fpaintings&q=' + searchItem + '&timerange=1month&access_token=' + accessToken, function (err, res, body) {
+            request('https://www.deviantart.com/api/v1/oauth2/browse/popular?category_path=digitalart%2Fpaintings&q=' + searchItem + '&timerange=1month&limit=8&access_token=' + accessToken, function (err, res, body) {
                 if (err) reject(err);
                 var json = JSON.parse(body);
                 resolve(json);
