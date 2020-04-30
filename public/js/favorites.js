227fb8750cc96d0173e21040ab5fdf4a89a473e1
Captain Sir <jakeovski@gmail.com>
2020-04-30 07:59:48 +0300

    next

--- public/js/favorites.js
+++ public/js/favorites.js
@@ -1,4 +1,10 @@
 function showFavorites() {
+    var y = document.getElementById("favorite");
+    if (y.style.backgroundColor === "#e0782e") {
+        y.style.backgroundColor = "#555";
+    }else{
+        y.style.backgroundColor = "#e0782e"
+    }
     var x = document.getElementById("show");
     if (x.style.display === "none") {
         x.style.display = "flex";
